8e9d55b23c3b7a2723b3f1f1732c55ff6e3b4852
David Boddie <david@boddie.org.uk>
2011-11-19 21:17:37 +0100

    Started working on another scrolling routine.

---
+++ build.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+
+"""
+Copyright (C) 2011 David Boddie <david@boddie.org.uk>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import os, struct, sys
+import UEFfile
+
+version = "0.1"
+
+def system(command):
+
+    if os.system(command):
+        sys.exit(1)
+
+def read_basic(path):
+
+    t = open(path).read()
+    t = t.replace("\n", "\r")
+    lines = t.rstrip().split("\r")
+    t = "\r".join(lines) + "\r"
+    return t
+
+def encode_text(text):
+
+    words = text.split(" ")
+    word_dict = {}
+    
+    # Count the number of occurrences of each word.
+    for word in words:
+        word_dict.setdefault(word, 0)
+        word_dict[word] += 1
+    
+    # Sort the words in order of decreasing frequency.
+    frequencies = map(lambda x: (x[1], x[0]), word_dict.items())
+    frequencies.sort()
+    frequencies.reverse()
+    
+    # Create encoding and decoding look up tables.
+    decoding_lookup = {}
+    encoding_lookup = {}
+    
+    i = 0
+    for count, word in frequencies:
+    
+        if i >= 128:
+            j = 1 + i * 2
+        else:
+            j = i * 2
+        
+        encoding_lookup[word] = j
+        decoding_lookup[j] = word
+        
+        i += 1
+    
+    # Encode the text.
+    encoded = []
+    for word in words:
+    
+        encoded.append(encoding_lookup[word])
+    
+    encoded_string = ""
+    for value in encoded:
+    
+        if value & 1 == 0:
+            encoded_string += chr(value)
+        else:
+            encoded_string += struct.pack("<H", value)
+    
+    return decoding_lookup, encoded_string
+
+def decode_text(data, lookup):
+
+    words = ""
+    i = 0
+    while i < len(data):
+    
+        value = ord(data[i])
+        if value & 1 != 0:
+            value += ord(data[i+1]) << 8
+            i += 2
+        else:
+            i += 1
+        
+        words += lookup[value]
+        words += " "
+    
+    return words[:-1]
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+    
+        sys.stderr.write("Usage: %s <new UEF file>\n" % sys.argv[0])
+        sys.exit(1)
+    
+    out_uef_file = sys.argv[1]
+    
+    files = []
+    
+    system("ophis code.oph CODE")
+    code = open("CODE").read()
+    code_start = 0x1900
+    files.append(("CODE", code_start, code_start, code))
+    
+    u = UEFfile.UEFfile(creator = 'build.py '+version)
+    u.minor = 6
+    u.target_machine = "Electron"
+    
+    u.import_files(0, files)
+    
+    # Insert a gap before each file.
+    offset = 0
+    for f in u.contents:
+    
+        # Insert a gap and some padding before the file.
+        gap_padding = [(0x112, "\xdc\x05"), (0x110, "\xdc\x05"), (0x100, "\xdc")]
+        u.chunks = u.chunks[:f["position"] + offset] + \
+                   gap_padding + u.chunks[f["position"] + offset:]
+
+        # Each time we insert a gap, the position of the file changes, so we
+        # need to update its position and last position. This isn't really
+        # necessary because we won't read the contents list again.
+        offset += len(gap_padding)
+        f["position"] += offset
+        f["last position"] += offset
+    
+    # Write the new UEF file.
+    try:
+        u.write(out_uef_file, write_emulator_info = False)
+    except UEFfile.UEFfile_error:
+        sys.stderr.write("Couldn't write the new executable to %s.\n" % out_uef_file)
+        sys.exit(1)
+
+    # Exit
+    sys.exit()
