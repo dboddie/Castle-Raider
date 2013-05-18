403438d16e0068ecd7257481e7372fe8b85ee27a
David Boddie <david@boddie.org.uk>
2013-05-18 18:53:19 +0200

    Removed the monster number restriction, introducing a restriction on the resulting value stored in the monster span data. Note that if this restriction is ignored, the monster simply won't appear.

--- tools/makelevels.py
+++ tools/makelevels.py
@@ -224,7 +224,9 @@ def create_level(levels_address, level_path, number_of_special_tiles):
         # Write spans to fill the space but only include a monster in the first
         # one.
         if monster > 0:
-            type_number = (y << 3) | (monster << 1)
+            type_number = (y << 3) | ((monster - 1) << 1)
+            if type_number == 0:
+                raise LevelError, "Resulting monster type is zero (monster = 0 and y = 0)."
         else:
             type_number = 0
         
