d0147eede51d1ade52a3631e8a1761e94bc4ae8a
David Boddie <david@boddie.org.uk>
2013-05-20 02:32:38 +0200

    Assigned more memory to the game code. Added support for half row movement of monsters. Fixed checks between monsters and scenery. Added a test structure to the level.

--- build.py
+++ build.py
@@ -146,7 +146,7 @@ if __name__ == "__main__":
     
     number_of_special_tiles = 32
     
-    data_start = 0x1f20
+    data_start = 0x1f60
     
     # Monster positions
     monster_positions_address       = data_start
